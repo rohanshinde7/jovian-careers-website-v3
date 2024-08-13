83652e1626e4a4ec97d2785d37fafa1d32d15fcd
rohanshinde7 <rohanarvindshinde@gmail.com>
2024-08-13 03:11:28 +0000

    Added Dynamic Pages to the website

--- app.py
+++ app.py
@@ -1,48 +1,26 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
-
-
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
-# JOBS = [{
-#   'id': 1,
-#   'title': 'Data Analyst',
-#   'location': 'Bengaluru, India',
-#   'salary': 'Rs. 10,00,000'
-# },
-# {
-#   'id': 2,
-#   'title': 'Data Scientist',
-#   'location': 'Delhi, India',
-#   'salary': 'Rs. 15,00,000'
-# },
-# {
-#   'id': 3,
-#   'title': 'Frontend Engineer',
-#   'location': 'Remote',
-  
-# },
-# {
-#   'id': 4,
-#   'title': 'Backend Engineer',
-#   'location': 'Remote',
-#   'salary': '$. 150,0000'
-# }
-# ]
-
-
 @app.route("/")
 def hello_jovian():
-  jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs)
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs = load_jobs_from_db()
-  return jsonify(jobs)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if job:
+        return render_template('jobpage.html',
+                               job = job)
+    else:
+        return "error: Job not found,  404"
 
-print(__name__)
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,19 +1,16 @@
 from sqlalchemy import create_engine, text
-
 import os
+
 db_connection_string = os.environ['DB_CONNECTION_STRING']
-
-
 
 # Create the engine with an SSL connection and increased timeout
 engine = create_engine(db_connection_string,
-           connect_args={"ssl": {
-               "ssl_ca": "/etc/ssl/cert.pem"
-           }})
-
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 def load_jobs_from_db():
-      with engine.connect() as conn:
+    with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
 
         jobs = []
@@ -22,16 +19,17 @@ def load_jobs_from_db():
             row_dict = dict(zip(result.keys(), row))
             jobs.append(row_dict)
 
+    return jobs
 
-      return jobs
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val": id}  # Pass the parameter as a dictionary
+        )
 
-
-    
-    # print("type(result):" , type(result))
-    # result_all = result.all()
-    # print("type(result.all()): ", type(result_all))
-    # first_result = result_all[0]
-    # print("type(first_result): ",type(first_result))
-    # first_result_dict = dict(result_all[0])
-    # print("type(first_result_dict): ",type(first_result_dict))
-    # print("first_result_dict: ",first_result_dict)
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(zip(result.keys(), rows[0]))
