fec70a6e136129550beca67643abb66caf03df86
rohanshinde7 <rohanarvindshinde@gmail.com>
2024-08-14 16:07:33 +0000

    Final HTML pages and connection to DB

--- database.py
+++ database.py
@@ -3,11 +3,14 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-# Create the engine with an SSL connection and increased timeout
-engine = create_engine(db_connection_string,
-                       connect_args={"ssl": {
-                           "ssl_ca": "/etc/ssl/cert.pem"
-                       }})
+engine = create_engine(
+    db_connection_string, 
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
@@ -33,3 +36,19 @@ def load_job_from_db(id):
             return None
         else:
             return dict(zip(result.keys(), rows[0]))
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) "
+                     "VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+        conn.execute(query, 
+                     {
+                         "job_id": job_id, 
+                         "full_name": data['full_name'],
+                         "email": data['email'],
+                         "linkedin_url": data['linkedin_url'],
+                         "education": data['education'],
+                         "work_experience": data['work_experience'],
+                         "resume_url": data['resume_url']
+                     })
